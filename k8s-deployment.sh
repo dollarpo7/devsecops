9dfbaeffaa5f763a617eede7f108e4689e228329
Adedolapo Okunsanmi <AdedolapoO@rystadenergy.com>
2026-03-12 17:02:36 +0100

    Fix: Reset k8s yaml before image substitution

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -2,6 +2,8 @@
 
 #k8s-deployment.sh
 
+# Reset the image placeholder before substituting (in case it was changed in a previous run)
+git checkout -- k8s_deployment_service.yaml 2>/dev/null || true
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 # kubectl -n default get deployment ${deploymentName} > /dev/null
 
